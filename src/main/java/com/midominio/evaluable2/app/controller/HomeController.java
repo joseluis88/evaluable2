c7956b2945a58040f67282da0d0bbf187d5c3e41
joseluis <moto_castrol@hotmail.com>
2023-08-07 09:13:52 +0200

    Añadido un botón para ver usuario y foto

--- src/main/java/com/midominio/evaluable2/app/controller/HomeController.java
+++ src/main/java/com/midominio/evaluable2/app/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
 	@GetMapping({ "/", "/home", "/home/" })
+	//@GetMapping({ "/homee" })
 	public String home() {
 		return "home";
 	}
